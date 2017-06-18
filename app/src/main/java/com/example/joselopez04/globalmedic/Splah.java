1278fc164cd49295905b59fbf7cf498eca1ee6b4
Jose Ruben Lopez Menjivar <jose.lopez04@telusinternational.com>
2017-06-18 10:43:23 -0600

    primer push medic app

---
+++ app/src/main/java/com/example/joselopez04/globalmedic/Splah.java
@@ -0,0 +1,35 @@
+package com.example.joselopez04.globalmedic;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.wangyuwei.particleview.ParticleView;
+
+public class Splah extends AppCompatActivity {
+
+    ParticleView mPvGithub;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splah);
+
+        mPvGithub = (ParticleView) findViewById(R.id.pv_1);
+
+        mPvGithub.startAnim();
+
+        mPvGithub.setOnParticleAnimListener(new ParticleView.ParticleAnimListener() {
+            @Override
+            public void onAnimationEnd() {
+                Intent intent = new Intent(Splah.this, HomeMenu.class);
+                Splah.this.startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
